e708a6f0040345c877ded0b76af4001d4a0eaadb
Saltzmann <opel.tobias@gmx.de>
2017-06-08 09:44:38 +0200

    FEAT: TO - working directory = .exe directory

--- CGAbschlussprojekt/main.cpp
+++ CGAbschlussprojekt/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    //Setzt Working-Directory auf .exe Ordner
+    //TODO richtige Aufrufstelle?
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
 
     return a.exec();
 }
