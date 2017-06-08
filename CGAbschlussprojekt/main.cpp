60c151e6acb7ca4deb95540aacc4079c4d53c0ec
Tobi <opel.tobias@gmx.de>
2017-06-08 17:53:00 +0200

    FEAT: TO - DebugContext in main eingebunden

--- CGAbschlussprojekt/main.cpp
+++ CGAbschlussprojekt/main.cpp
@@ -1,9 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSurfaceFormat>
 #include <QDir>
 
 int main(int argc, char *argv[])
 {
+    QSurfaceFormat format;
+    format.setVersion(4, 4); //allgemein kompatible OpenGL Version
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    //format.setProfile(QSurfaceFormat::StereoBuffers); //wenn das double buffering aktiviert wäre das gut - erst einkommentieren wenn Rest funktioniert
+    //format.setProfile(QSurfaceFormat::CompatibilityProfile); // müssen wir testen
+    //format.setOption(QSurfaceFormat::DeprecatedFunctions); // ob wir dies brauchen
+    format.setOption(QSurfaceFormat::DebugContext);
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
